1261a6950a581bbc8efe5583822f725a50953792
Matan Amidor <matan.amidor@gmail.com>
2021-11-08 17:37:01 +0100

    works but can be cheated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,6 @@
 require 'json'
 require 'open-uri'
-
-
-
 class GamesController < ApplicationController
-
   def new
     @letters = Array.new(10) { [*'A'..'Z'].sample }
   end
@@ -13,14 +9,14 @@ class GamesController < ApplicationController
     @answer = params[:word]
     url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
     @score = JSON.parse(open(url).read)
-    split_letters = @answer.split('')
-    letters = params['letters'].split('')
-    @return_phrase = if !@score['found']
-                       'The word you entered is not in ENGLISH!'
-                     elsif !(split_letters - letters).empty?
-                       'Youre word SUCK! its not made out of the letter i gave you!'
-                     else
-                       'You Rock!'
-                     end
+    # split_letters = @answer.upcase.split('')
+    # letters = params['letters'].split('')
+    if !@score['found']
+      @return_phrase = "I\'m sorry but #{@answer} is not in ENGLISH!"
+    elsif params[:letters].include?(@answer)
+      @return_phrase = "You should have used only #{letters.join('')} and #{@answer} does not cut it! Try again!"
+    else
+      @return_phrase = 'You Rock!'
+    end
   end
 end
